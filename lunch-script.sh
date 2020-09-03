18e888b24f5109e77a90f26808f14bfbb579e37c
Digo Gonzalez <diego.ags04@gmail.com>
2020-09-03 19:00:44 -0400

    added script lunch

---
+++ lunch-script.sh
@@ -0,0 +1,7 @@
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 12.13.0
+
+# now node works
+node ~/hermanospc-automated-news/index.js
