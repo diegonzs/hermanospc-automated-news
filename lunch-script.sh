bd6cca584452069273f23f42fbf5106b1dda9fef
Digo Gonzalez <diego.ags04@gmail.com>
2020-09-03 21:08:04 -0400

    changed

--- lunch-script.sh
+++ lunch-script.sh
@@ -1,7 +1,7 @@
 # Load nvm
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm use stable
+nvm use v10.17.0
 
 # now node works
 node ~/hermanospc-automated-news/index.js
